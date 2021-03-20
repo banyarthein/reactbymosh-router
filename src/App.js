998b17a71eace80bc4a3c60a96bddf0b7e8e217a
banyarthein <banyarthein@gmail.com>
2021-03-20 14:19:30 +0800

    addd first route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {Route} from "react-router-dom";
+
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -6,6 +8,7 @@ import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
+
 import "./App.css";
 
 class App extends Component {
@@ -13,6 +16,12 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+            <Route path="/products" component={Products}></Route>
+            <Route path="/posts" component={Posts}></Route>
+            <Route path="/admin" component={Dashboard}></Route>
+            <Route path="/" component={Home}></Route>
+        </div>
       </div>
     );
   }
